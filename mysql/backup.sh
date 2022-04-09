bd40273b8af34f5a0e5ce144b3c35a16045fb14a
zhoujunhe <13805718733@139.com>
2022-04-09 22:10:26 +0800

    v1.1

--- mysql/backup.sh
+++ mysql/backup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # 备份数据
-docker run --rm -v /opt/mysql-redis/mysql/backup:/backup --volumes-from mysql percona/percona-xtrabackup:8.0 xtrabackup --backup --compress --datadir=/var/lib/mysql/ --target-dir=/backup/`date '+%Y%m%d'` --user=root --password=00#Abc#123 
+docker run --rm -v `dirname $0`/backup:/backup --volumes-from mysql percona/percona-xtrabackup:8.0 xtrabackup --backup --compress --datadir=/var/lib/mysql/ --target-dir=/backup/`date '+%Y%m%d'` --user=root --password=00#Abc#123 
 # 检测备份是否成功，成功删除昨天的备份
 if [ $? -eq 0 ]; then
-	rm -rf /opt/mysql-redis/mysql/backup/`date -d"yesterday" +%Y%m%d`
+	rm -rf `dirname $0`/backup/`date -d"yesterday" +%Y%m%d`
 fi
